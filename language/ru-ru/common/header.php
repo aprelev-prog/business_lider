9a57fe0b3afb54019070e258a82ded10b9f5adc6
aprelev-prog <120953318+aprelev-prog@users.noreply.github.com>
2023-04-14 00:07:56 +0400

    test-task
    test-task

---
+++ language/ru-ru/common/header.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+$_['text_tittle']                  =     'Тестовое задание ';
+$_['text_city']                    =     'Санкт-Петербург';
+$_['text_catalog']                 =     'Каталог';
+$_['text_shipping_payment']        =     'Доставка и оплата';
+$_['text_company']                 =     'О компании';
+$_['text_contacts']                =     'Контакты';
+$_['text_stock']                   =     'Акции';
+$_['text_attractions']             =     'Достопримечательности';
+$_['text_number']                  =     '8 (800) 535 44 44';
+
